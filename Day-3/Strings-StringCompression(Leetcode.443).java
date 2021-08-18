391bec23f8f3660b85d65ffeba22eaa35fd67775
rahulsinghnirban <80592933+rahulsinghnirban@users.noreply.github.com>
2021-08-19 00:11:40 +0530

    Create Strings-StringCompression(Leetcode.443).java

---
+++ Day-3/Strings-StringCompression(Leetcode.443).java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compress(char[] chars) {
+        
+        int index = 0;
+        int i=0;
+        
+        while(i<chars.length){
+            int j = i;
+            
+            while(j<chars.length && chars[i] == chars[j]){
+                j++;
+            }
+            
+            chars[index++] = chars[i];
+            
+            if(j - i > 1){
+                String count = j - i + ""; //2
+                
+                for(char c: count.toCharArray()){
+                    chars[index++] = c;
+                }
+            }
+            i = j;
+        }
+        return index;
+    }
+}
